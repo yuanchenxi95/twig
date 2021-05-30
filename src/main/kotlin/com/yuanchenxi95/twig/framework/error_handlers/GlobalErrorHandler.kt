ae8b798945c06bd5a5e7be598ba5d37f02010f97
ZhouxiyuJiang <jiangzhouxy@gmail.com>
2021-05-30 15:39:23 -0700

    Add Sentry Spring-boot support

--- src/main/kotlin/com/yuanchenxi95/twig/framework/error_handlers/GlobalErrorHandler.kt
+++ src/main/kotlin/com/yuanchenxi95/twig/framework/error_handlers/GlobalErrorHandler.kt
@@ -2,6 +2,7 @@ package com.yuanchenxi95.twig.framework.error_handlers
 
 import com.yuanchenxi95.twig.constants.DEFAULT_TWIG_INTERNAL_ERROR
 import com.yuanchenxi95.twig.framework.codecs.encodeProtobufValue
+import io.sentry.Sentry
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -13,6 +14,7 @@ import reactor.core.publisher.Mono
 class GlobalErrorHandler : ErrorWebExceptionHandler {
     override fun handle(serverWebExchange: ServerWebExchange, exception: Throwable): Mono<Void> {
         print(exception)
+        Sentry.captureException(exception)
         val response = serverWebExchange.response
         val bufferFactory = response.bufferFactory()
         response.headers.contentType = MediaType.APPLICATION_JSON
