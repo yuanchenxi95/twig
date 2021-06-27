02d55b4858b28e04b507f924ad0a610d46cd9aef
Chenxi Yuan <yuanchenxi95@gmail.com>
2021-06-27 16:39:33 -0700

    Changes tag to be user level.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,3 @@
-create table if not exists tag
-(
-    id          varchar(36)                         not null
-        primary key,
-    tag_name    varchar(255)                        not null,
-    create_time timestamp default CURRENT_TIMESTAMP not null,
-    constraint tag_name
-        unique (tag_name),
-    constraint tag_pk_2
-        unique (tag_name)
-);
-
-create index tag_create_time_index
-    on tag (create_time desc);
-
 create table if not exists url
 (
     id          varchar(36)                         not null
@@ -62,10 +47,11 @@ create index _expiration_time_index
     on session (expiration_time desc);
 
 create event expiresession
-on schedule every 1 day
-do
-delete from session
-where expiration_time < now()
+    on schedule every 1 day
+    do
+    delete
+    from session
+    where expiration_time < now()
 ;
 
 create table if not exists bookmark
@@ -88,6 +74,24 @@ create index _update_time_index
 create index bookmark_create_time_index
     on bookmark (create_time desc);
 
+create table if not exists tag
+(
+    id          varchar(36)                         not null
+        primary key,
+    tag_name    varchar(255)                        not null,
+    user_id     varchar(36)                         not null,
+    create_time timestamp default CURRENT_TIMESTAMP not null,
+    constraint tag_name
+        unique (tag_name),
+    constraint tag_pk_2
+        unique (tag_name),
+    constraint tag_user_id_fk
+        foreign key (user_id) references user (id)
+);
+
+create index tag_create_time_index
+    on tag (create_time desc);
+
 create table if not exists tags_bookmarks
 (
     id          varchar(36)                         not null
@@ -104,25 +108,8 @@ create table if not exists tags_bookmarks
 create index tags_bookmarks_create_time_index
     on tags_bookmarks (create_time desc);
 
-create table if not exists tags_users
-(
-    id          varchar(36)                         not null
-        primary key,
-    user_id     varchar(36)                         not null,
-    tag_id      varchar(36)                         not null,
-    create_time timestamp default CURRENT_TIMESTAMP not null,
-    constraint tags_users_tag_id_fk
-        foreign key (tag_id) references tag (id),
-    constraint tags_users_user_id_fk
-        foreign key (user_id) references user (id)
-);
-
-create index tags_users_create_time_index
-    on tags_users (create_time desc);
-
 create index user_create_time_index
     on user (create_time desc);
 
 create index user_update_time_index
     on user (update_time desc);
-
